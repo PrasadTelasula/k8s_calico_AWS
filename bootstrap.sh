c4c85d8f9c2100f7479bb4285d878743c6e72db0
PrasadTelasula <prasad.telasula@yahoo.com>
2020-02-28 20:51:22 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 # Install docker from Docker-ce repository
 echo "[TASK 1] Install docker container engine"
+yum install wget vim git -y
 yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
 yum install -y -q docker-ce >/dev/null 2>&1
